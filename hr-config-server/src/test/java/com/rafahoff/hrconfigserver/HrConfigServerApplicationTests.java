3842890bf579645f0d079ff3ac49037536f39197
Rafa-Hoff <rafahoff32@gmail.com>
2023-10-03 16:00:56 -0400

    Project hr-config-server created

---
+++ hr-config-server/src/test/java/com/rafahoff/hrconfigserver/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafahoff.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
