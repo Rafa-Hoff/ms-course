0b5596cc7b4f0b4f24dd7a44dbbbcf23343b1d20
Rafa-Hoff <rafahoff32@gmail.com>
2023-10-13 16:30:41 -0400

    UserRepository, UserResource, Zuul config

---
+++ hr-user/src/main/java/com/rafahoff/hruser/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.rafahoff.hruser.repositories;
+
+import com.rafahoff.hruser.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+}
